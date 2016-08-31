38e051de39a68f8efb71aa0d31d8a42f1800d4c7
mrxiaobin <mrxiaobin2011@gmail.com>
2016-08-31 09:42:58 +0800

    Initial Commit

---
+++ Touch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Touch
+//
+//  Created by zhuxiaobin on 16/8/31.
+//  Copyright © 2016年 zhuxiaobin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Touch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Touch
+//
+//  Created by zhuxiaobin on 16/8/31.
+//  Copyright © 2016年 zhuxiaobin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
